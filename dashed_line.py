aa65f0659a5d4879c848dd7dcfc13eaa3c71324d
Aditya Vyavahare <vvaditya23@icloud.com>
2023-11-17 21:24:20 +0530

    drew dashed line

--- dashed_line.py
+++ dashed_line.py
@@ -4,9 +4,9 @@ tt_turtle_obj = Turtle()
 
 for _ in range(15):
     tt_turtle_obj.forward(10)
-    tt_turtle_obj.penup()
+    tt_turtle_obj.penup()   #pen is up that means nothing will be written
     tt_turtle_obj.forward(10)
-    tt_turtle_obj.pendown()
+    tt_turtle_obj.pendown() #pen down, writing is on
 
 screen = Screen()
 screen.exitonclick()
