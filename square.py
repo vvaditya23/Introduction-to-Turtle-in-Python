cdf3ae85c054b07065f5eb698d14f90bfc94482e
Aditya Vyavahare <vvaditya23@icloud.com>
2023-11-17 20:08:14 +0530

    drew square using turtle

---
+++ square.py
@@ -0,0 +1,18 @@
+from turtle import  Turtle, Screen
+
+timy_the_turtle = Turtle()
+
+timy_the_turtle.shape("turtle")
+timy_the_turtle.forward(200)
+timy_the_turtle.right(90)
+
+timy_the_turtle.forward(200)
+timy_the_turtle.right(90)
+
+timy_the_turtle.forward(200)
+timy_the_turtle.right(90)
+
+timy_the_turtle.forward(200)
+
+screen = Screen()
+screen.exitonclick()
